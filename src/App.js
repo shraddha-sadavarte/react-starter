d03575f6a1a48492bb0980dc2f23d7bc98403ded
shraddha-sadavarte <shraddhasadavarte24@gmail.com>
2022-11-24 15:04:15 -0800

    fries added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Pizza from './Pizza.js'
 import Burger from './Burger.js'
+import Fries from './Fries.js'
 
 function App() {
   return (
     <>
         <Pizza />
         <Burger/>
+        <Fries/>
     </>
   );
 }
---
+++ src/Fries.js
@@ -0,0 +1,13 @@
+import './Pizza.css';
+function Fries(){
+    return(
+        <>
+           <div className='container-pizza'>
+              <h1>Fries🍟🍟🍟</h1>
+              <span className='container-item-pizza'>Feel the Crispiness...🤩😊</span>
+              <h2 className='name'>₨.100/-</h2>
+           </div>
+        </>
+    )
+}
+export default Fries;
