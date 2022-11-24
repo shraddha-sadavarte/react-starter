d1966c30c0f727ae730026a1574ee199e217d6ff
shraddha-sadavarte <shraddhasadavarte24@gmail.com>
2022-11-24 13:59:05 -0800

    burger modified

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Pizza from './Pizza.js'
+import Burger from './Burger.js'
 
 function App() {
   return (
     <>
         <Pizza />
+        <Burger/>
     </>
   );
 }
--- src/Burger.js
+++ src/Burger.js
@@ -2,7 +2,7 @@ import './Pizza.css';
 function Burger(){
     return(
         <>
-           <div className='container-burger'>
+           <div className='container-pizza'>
               <h1>Burger🍔🍔🍔</h1>
               <span className='container-item-pizza'>Glorius Heavenly Burgers...🤩😊</span>
               <h2 className='name'>₨.150/-</h2>
